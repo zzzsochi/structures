d406b33279c09dc6a57eb52f78b6d4d6cc6616c7
Alexander Zelenyak <zzz.sochi@gmail.com>
2014-03-19 17:37:38 +0400

    Fix setup.py

--- setup.py
+++ setup.py
@@ -10,24 +10,22 @@ long_description = open('README.rst', 'rb').read().decode('utf8')
 
 
 setup(
-        name='structures',
-        version=__version__,
-        description=description,
-        long_description=long_description,
-        author='Zelenyak Aleksandr aka ZZZ',
-        author_email='ZZZ.Sochi@GMail.com',
-        url='https://github.com/zzzsochi/structures',
-        license='GPL',
-        platforms='any',
+    name='structures',
+    version=__version__,
+    description=description,
+    long_description=long_description,
+    author='Zelenyak Aleksander aka ZZZ',
+    author_email='zzz.sochi@gmail.com',
+    url='https://github.com/zzzsochi/structures',
+    license='GPL',
+    platforms='any',
 
-        classifiers=[
-                'Development Status :: 5 - Production/Stable',
-                'Intended Audience :: Developers',
-                'Operating System :: OS Independent',
-                'Programming Language :: Python :: 3',
-                'Programming Language :: Python :: 3.2',
-                'Programming Language :: Python :: 3.3',
-              ],
+    classifiers=[
+            'Development Status :: 5 - Production/Stable',
+            'Intended Audience :: Developers',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python :: 3',
+          ],
 
-        packages=['structures'],
-    )
+    packages=['structures'],
+)
